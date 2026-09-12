cc1c3866af2d50d9047b360edf5876df0e8dddd0
Limette45 <heyeyoushang39@gmail.com>
2026-09-13 01:03:51 +0900

    Create verbs.js

---
+++ verbs.js
@@ -0,0 +1,1042 @@
+const verbs = {
+
+  estar: {
+    presente: {
+      yo: "estoy",
+      tú: "estás",
+      él: "está",
+      nosotros: "estamos",
+      ellos: "están"
+    },
+
+    futuro: {
+      yo: "estaré",
+      tú: "estarás",
+      él: "estará",
+      nosotros: "estaremos",
+      ellos: "estarán"
+    },
+
+    preterito: {
+      yo: "estuve",
+      tú: "estuviste",
+      él: "estuvo",
+      nosotros: "estuvimos",
+      ellos: "estuvieron"
+    },
+
+    imperfecto: {
+      yo: "estaba",
+      tú: "estabas",
+      él: "estaba",
+      nosotros: "estábamos",
+      ellos: "estaban"
+    },
+
+    condicional: {
+      yo: "estaría",
+      tú: "estarías",
+      él: "estaría",
+      nosotros: "estaríamos",
+      ellos: "estarían"
+    },
+
+    subjuntivoPresente: {
+      yo: "esté",
+      tú: "estés",
+      él: "esté",
+      nosotros: "estemos",
+      ellos: "estén"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "estuviera",
+      tú: "estuvieras",
+      él: "estuviera",
+      nosotros: "estuviéramos",
+      ellos: "estuvieran"
+    },
+
+    extra: {
+      gerundio: "estando",
+      participio: "estado",
+      imperativoTú: "está"
+    }
+  },
+
+
+  ser: {
+    presente: {
+      yo: "soy",
+      tú: "eres",
+      él: "es",
+      nosotros: "somos",
+      ellos: "son"
+    },
+
+    futuro: {
+      yo: "seré",
+      tú: "serás",
+      él: "será",
+      nosotros: "seremos",
+      ellos: "serán"
+    },
+
+    preterito: {
+      yo: "fui",
+      tú: "fuiste",
+      él: "fue",
+      nosotros: "fuimos",
+      ellos: "fueron"
+    },
+
+    imperfecto: {
+      yo: "era",
+      tú: "eras",
+      él: "era",
+      nosotros: "éramos",
+      ellos: "eran"
+    },
+
+    condicional: {
+      yo: "sería",
+      tú: "serías",
+      él: "sería",
+      nosotros: "seríamos",
+      ellos: "serían"
+    },
+
+    subjuntivoPresente: {
+      yo: "sea",
+      tú: "seas",
+      él: "sea",
+      nosotros: "seamos",
+      ellos: "sean"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "fuera",
+      tú: "fueras",
+      él: "fuera",
+      nosotros: "fuéramos",
+      ellos: "fueran"
+    },
+
+    extra: {
+      gerundio: "siendo",
+      participio: "sido",
+      imperativoTú: "sé"
+    }
+  },
+
+
+  tener: {
+    presente: {
+      yo: "tengo",
+      tú: "tienes",
+      él: "tiene",
+      nosotros: "tenemos",
+      ellos: "tienen"
+    },
+
+    futuro: {
+      yo: "tendré",
+      tú: "tendrás",
+      él: "tendrá",
+      nosotros: "tendremos",
+      ellos: "tendrán"
+    },
+
+    preterito: {
+      yo: "tuve",
+      tú: "tuviste",
+      él: "tuvo",
+      nosotros: "tuvimos",
+      ellos: "tuvieron"
+    },
+
+    imperfecto: {
+      yo: "tenía",
+      tú: "tenías",
+      él: "tenía",
+      nosotros: "teníamos",
+      ellos: "tenían"
+    },
+
+    condicional: {
+      yo: "tendría",
+      tú: "tendrías",
+      él: "tendría",
+      nosotros: "tendríamos",
+      ellos: "tendrían"
+    },
+
+    subjuntivoPresente: {
+      yo: "tenga",
+      tú: "tengas",
+      él: "tenga",
+      nosotros: "tengamos",
+      ellos: "tengan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "tuviera",
+      tú: "tuvieras",
+      él: "tuviera",
+      nosotros: "tuviéramos",
+      ellos: "tuvieran"
+    },
+
+    extra: {
+      gerundio: "teniendo",
+      participio: "tenido",
+      imperativoTú: "ten"
+    }
+  },
+
+
+  haber: {
+    presente: {
+      yo: "he",
+      tú: "has",
+      él: "ha",
+      nosotros: "hemos",
+      ellos: "han"
+    },
+
+    futuro: {
+      yo: "habré",
+      tú: "habrás",
+      él: "habrá",
+      nosotros: "habremos",
+      ellos: "habrán"
+    },
+
+    preterito: {
+      yo: "hube",
+      tú: "hubiste",
+      él: "hubo",
+      nosotros: "hubimos",
+      ellos: "hubieron"
+    },
+
+    imperfecto: {
+      yo: "había",
+      tú: "habías",
+      él: "había",
+      nosotros: "habíamos",
+      ellos: "habían"
+    },
+
+    condicional: {
+      yo: "habría",
+      tú: "habrías",
+      él: "habría",
+      nosotros: "habríamos",
+      ellos: "habrían"
+    },
+
+    subjuntivoPresente: {
+      yo: "haya",
+      tú: "hayas",
+      él: "haya",
+      nosotros: "hayamos",
+      ellos: "hayan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "hubiera",
+      tú: "hubieras",
+      él: "hubiera",
+      nosotros: "hubiéramos",
+      ellos: "hubieran"
+    },
+
+    extra: {
+      gerundio: "habiendo",
+      participio: "habido",
+      imperativoTú: "he"
+    }
+  },
+
+
+  hacer: {
+    presente: {
+      yo: "hago",
+      tú: "haces",
+      él: "hace",
+      nosotros: "hacemos",
+      ellos: "hacen"
+    },
+
+    futuro: {
+      yo: "haré",
+      tú: "harás",
+      él: "hará",
+      nosotros: "haremos",
+      ellos: "harán"
+    },
+
+    preterito: {
+      yo: "hice",
+      tú: "hiciste",
+      él: "hizo",
+      nosotros: "hicimos",
+      ellos: "hicieron"
+    },
+
+    imperfecto: {
+      yo: "hacía",
+      tú: "hacías",
+      él: "hacía",
+      nosotros: "hacíamos",
+      ellos: "hacían"
+    },
+
+    condicional: {
+      yo: "haría",
+      tú: "harías",
+      él: "haría",
+      nosotros: "haríamos",
+      ellos: "harían"
+    },
+
+    subjuntivoPresente: {
+      yo: "haga",
+      tú: "hagas",
+      él: "haga",
+      nosotros: "hagamos",
+      ellos: "hagan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "hiciera",
+      tú: "hicieras",
+      él: "hiciera",
+      nosotros: "hiciéramos",
+      ellos: "hicieran"
+    },
+
+    extra: {
+      gerundio: "haciendo",
+      participio: "hecho",
+      imperativoTú: "haz"
+    }
+  },
+
+
+  poder: {
+    presente: {
+      yo: "puedo",
+      tú: "puedes",
+      él: "puede",
+      nosotros: "podemos",
+      ellos: "pueden"
+    },
+
+    futuro: {
+      yo: "podré",
+      tú: "podrás",
+      él: "podrá",
+      nosotros: "podremos",
+      ellos: "podrán"
+    },
+
+    preterito: {
+      yo: "pude",
+      tú: "pudiste",
+      él: "pudo",
+      nosotros: "pudimos",
+      ellos: "pudieron"
+    },
+
+    imperfecto: {
+      yo: "podía",
+      tú: "podías",
+      él: "podía",
+      nosotros: "podíamos",
+      ellos: "podían"
+    },
+
+    condicional: {
+      yo: "podría",
+      tú: "podrías",
+      él: "podría",
+      nosotros: "podríamos",
+      ellos: "podrían"
+    },
+
+    subjuntivoPresente: {
+      yo: "pueda",
+      tú: "puedas",
+      él: "pueda",
+      nosotros: "podamos",
+      ellos: "puedan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "pudiera",
+      tú: "pudieras",
+      él: "pudiera",
+      nosotros: "pudiéramos",
+      ellos: "pudieran"
+    },
+
+    extra: {
+      gerundio: "pudiendo",
+      participio: "podido",
+      imperativoTú: "puede"
+    }
+  },
+
+
+  decir: {
+    presente: {
+      yo: "digo",
+      tú: "dices",
+      él: "dice",
+      nosotros: "decimos",
+      ellos: "dicen"
+    },
+
+    futuro: {
+      yo: "diré",
+      tú: "dirás",
+      él: "dirá",
+      nosotros: "diremos",
+      ellos: "dirán"
+    },
+
+    preterito: {
+      yo: "dije",
+      tú: "dijiste",
+      él: "dijo",
+      nosotros: "dijimos",
+      ellos: "dijeron"
+    },
+
+    imperfecto: {
+      yo: "decía",
+      tú: "decías",
+      él: "decía",
+      nosotros: "decíamos",
+      ellos: "decían"
+    },
+
+    condicional: {
+      yo: "diría",
+      tú: "dirías",
+      él: "diría",
+      nosotros: "diríamos",
+      ellos: "dirían"
+    },
+
+    subjuntivoPresente: {
+      yo: "diga",
+      tú: "digas",
+      él: "diga",
+      nosotros: "digamos",
+      ellos: "digan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "dijera",
+      tú: "dijeras",
+      él: "dijera",
+      nosotros: "dijéramos",
+      ellos: "dijeran"
+    },
+
+    extra: {
+      gerundio: "diciendo",
+      participio: "dicho",
+      imperativoTú: "di"
+    }
+  },
+
+
+  ir: {
+    presente: {
+      yo: "voy",
+      tú: "vas",
+      él: "va",
+      nosotros: "vamos",
+      ellos: "van"
+    },
+
+    futuro: {
+      yo: "iré",
+      tú: "irás",
+      él: "irá",
+      nosotros: "iremos",
+      ellos: "irán"
+    },
+
+    preterito: {
+      yo: "fui",
+      tú: "fuiste",
+      él: "fue",
+      nosotros: "fuimos",
+      ellos: "fueron"
+    },
+
+    imperfecto: {
+      yo: "iba",
+      tú: "ibas",
+      él: "iba",
+      nosotros: "íbamos",
+      ellos: "iban"
+    },
+
+    condicional: {
+      yo: "iría",
+      tú: "irías",
+      él: "iría",
+      nosotros: "iríamos",
+      ellos: "irían"
+    },
+
+    subjuntivoPresente: {
+      yo: "vaya",
+      tú: "vayas",
+      él: "vaya",
+      nosotros: "vayamos",
+      ellos: "vayan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "fuera",
+      tú: "fueras",
+      él: "fuera",
+      nosotros: "fuéramos",
+      ellos: "fueran"
+    },
+
+    extra: {
+      gerundio: "yendo",
+      participio: "ido",
+      imperativoTú: "ve"
+    }
+  },
+
+
+  dar: {
+    presente: {
+      yo: "doy",
+      tú: "das",
+      él: "da",
+      nosotros: "damos",
+      ellos: "dan"
+    },
+
+    futuro: {
+      yo: "daré",
+      tú: "darás",
+      él: "dará",
+      nosotros: "daremos",
+      ellos: "darán"
+    },
+
+    preterito: {
+      yo: "di",
+      tú: "diste",
+      él: "dio",
+      nosotros: "dimos",
+      ellos: "dieron"
+    },
+
+    imperfecto: {
+      yo: "daba",
+      tú: "dabas",
+      él: "daba",
+      nosotros: "dábamos",
+      ellos: "daban"
+    },
+
+    condicional: {
+      yo: "daría",
+      tú: "darías",
+      él: "daría",
+      nosotros: "daríamos",
+      ellos: "darían"
+    },
+
+    subjuntivoPresente: {
+      yo: "dé",
+      tú: "des",
+      él: "dé",
+      nosotros: "demos",
+      ellos: "den"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "diera",
+      tú: "dieras",
+      él: "diera",
+      nosotros: "diéramos",
+      ellos: "dieran"
+    },
+
+    extra: {
+      gerundio: "dando",
+      participio: "dado",
+      imperativoTú: "da"
+    }
+  },
+
+
+  hablar: {
+    presente: {
+      yo: "hablo",
+      tú: "hablas",
+      él: "habla",
+      nosotros: "hablamos",
+      ellos: "hablan"
+    },
+
+    futuro: {
+      yo: "hablaré",
+      tú: "hablarás",
+      él: "hablará",
+      nosotros: "hablaremos",
+      ellos: "hablarán"
+    },
+
+    preterito: {
+      yo: "hablé",
+      tú: "hablaste",
+      él: "habló",
+      nosotros: "hablamos",
+      ellos: "hablaron"
+    },
+
+    imperfecto: {
+      yo: "hablaba",
+      tú: "hablabas",
+      él: "hablaba",
+      nosotros: "hablábamos",
+      ellos: "hablaban"
+    },
+
+    condicional: {
+      yo: "hablaría",
+      tú: "hablarías",
+      él: "hablaría",
+      nosotros: "hablaríamos",
+      ellos: "hablarían"
+    },
+
+    subjuntivoPresente: {
+      yo: "hable",
+      tú: "hables",
+      él: "hable",
+      nosotros: "hablemos",
+      ellos: "hablen"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "hablara",
+      tú: "hablaras",
+      él: "hablara",
+      nosotros: "habláramos",
+      ellos: "hablaran"
+    },
+
+    extra: {
+      gerundio: "hablando",
+      participio: "hablado",
+      imperativoTú: "habla"
+    }
+  },
+
+
+  comer: {
+    presente: {
+      yo: "como",
+      tú: "comes",
+      él: "come",
+      nosotros: "comemos",
+      ellos: "comen"
+    },
+
+    futuro: {
+      yo: "comeré",
+      tú: "comerás",
+      él: "comerá",
+      nosotros: "comeremos",
+      ellos: "comerán"
+    },
+
+    preterito: {
+      yo: "comí",
+      tú: "comiste",
+      él: "comió",
+      nosotros: "comimos",
+      ellos: "comieron"
+    },
+
+    imperfecto: {
+      yo: "comía",
+      tú: "comías",
+      él: "comía",
+      nosotros: "comíamos",
+      ellos: "comían"
+    },
+
+    condicional: {
+      yo: "comería",
+      tú: "comerías",
+      él: "comería",
+      nosotros: "comeríamos",
+      ellos: "comerían"
+    },
+
+    subjuntivoPresente: {
+      yo: "coma",
+      tú: "comas",
+      él: "coma",
+      nosotros: "comamos",
+      ellos: "coman"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "comiera",
+      tú: "comieras",
+      él: "comiera",
+      nosotros: "comiéramos",
+      ellos: "comieran"
+    },
+
+    extra: {
+      gerundio: "comiendo",
+      participio: "comido",
+      imperativoTú: "come"
+    }
+  },
+
+
+  vivir: {
+    presente: {
+      yo: "vivo",
+      tú: "vives",
+      él: "vive",
+      nosotros: "vivimos",
+      ellos: "viven"
+    },
+
+    futuro: {
+      yo: "viviré",
+      tú: "vivirás",
+      él: "vivirá",
+      nosotros: "viviremos",
+      ellos: "vivirán"
+    },
+
+    preterito: {
+      yo: "viví",
+      tú: "viviste",
+      él: "vivió",
+      nosotros: "vivimos",
+      ellos: "vivieron"
+    },
+
+    imperfecto: {
+      yo: "vivía",
+      tú: "vivías",
+      él: "vivía",
+      nosotros: "vivíamos",
+      ellos: "vivían"
+    },
+
+    condicional: {
+      yo: "viviría",
+      tú: "vivirías",
+      él: "viviría",
+      nosotros: "viviríamos",
+      ellos: "vivirían"
+    },
+
+    subjuntivoPresente: {
+      yo: "viva",
+      tú: "vivas",
+      él: "viva",
+      nosotros: "vivamos",
+      ellos: "vivan"
+    },
+
+    subjuntivoImperfecto: {
+      yo: "viviera",
+      tú: "vivieras",
+      él: "viviera",
+      nosotros: "viviéramos",
+      ellos: "vivieran"
+    },
+
+    extra: {
+      gerundio: "viviendo",
+      participio: "vivido",
+      imperativoTú: "vive"
+    }
+  }
+
+};
+
+
+
+// ================================
+// 問題カテゴリー
+// ================================
+
+const questionTypes = [
+
+  {
+    key: "presente",
+    label: "現在"
+  },
+
+  {
+    key: "futuro",
+    label: "未来"
+  },
+
+  {
+    key: "preterito",
+    label: "点過去"
+  },
+
+  {
+    key: "imperfecto",
+    label: "線過去"
+  },
+
+  {
+    key: "condicional",
+    label: "条件法"
+  },
+
+  {
+    key: "subjuntivoPresente",
+    label: "接続法現在"
+  },
+
+  {
+    key: "subjuntivoImperfecto",
+    label: "接続法過去 (-ra)"
+  }
+
+];
+
+
+
+const persons = [
+  "yo",
+  "tú",
+  "él",
+  "nosotros",
+  "ellos"
+];
+
+
+
+const extraQuestions = [
+
+  {
+    key: "gerundio",
+    label: "現在分詞"
+  },
+
+  {
+    key: "participio",
+    label: "過去分詞"
+  },
+
+  {
+    key: "imperativoTú",
+    label: "tú 命令形"
+  }
+
+];
+
+
+
+// ================================
+// HTML要素
+// ================================
+
+const verbSelect = document.getElementById("verbSelect");
+
+const verbDisplay = document.getElementById("verbDisplay");
+
+const questionDisplay = document.getElementById("questionDisplay");
+
+const answerBox = document.getElementById("answerBox");
+
+const answerDisplay = document.getElementById("answerDisplay");
+
+const clickText = document.getElementById("clickText");
+
+const nextButton = document.getElementById("nextButton");
+
+
+
+let currentAnswer = "";
+let answerVisible = false;
+
+
+
+// ================================
+// ランダム選択
+// ================================
+
+function randomItem(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+
+
+// ================================
+// 新しい問題を作る
+// ================================
+
+function generateQuestion() {
+
+  // 答えを隠す
+  answerVisible = false;
+
+  answerDisplay.classList.add("hidden");
+  nextButton.classList.add("hidden");
+
+  clickText.classList.remove("hidden");
+
+
+  // 動詞を決定
+
+  let selectedVerb = verbSelect.value;
+  let verbName;
+
+  if (selectedVerb === "random") {
+
+    const verbNames = Object.keys(verbs);
+
+    verbName = randomItem(verbNames);
+
+  } else {
+
+    verbName = selectedVerb;
+
+  }
+
+
+  const verbData = verbs[verbName];
+
+
+  // 通常の時制 + extra を合わせる
+
+  const allQuestionGroups = [
+    ...questionTypes,
+    ...extraQuestions
+  ];
+
+
+  const questionGroup = randomItem(allQuestionGroups);
+
+
+  // 動詞表示
+
+  verbDisplay.textContent = verbName;
+
+
+  // ================================
+  // 現在分詞・過去分詞・命令形
+  // ================================
+
+  if (
+    questionGroup.key === "gerundio" ||
+    questionGroup.key === "participio" ||
+    questionGroup.key === "imperativoTú"
+  ) {
+
+    questionDisplay.textContent = questionGroup.label;
+
+    currentAnswer =
+      verbData.extra[questionGroup.key];
+
+  }
+
+
+  // ================================
+  // 通常の活用
+  // ================================
+
+  else {
+
+    const person = randomItem(persons);
+
+    questionDisplay.textContent =
+      `${questionGroup.label}: ${person}`;
+
+    currentAnswer =
+      verbData[questionGroup.key][person];
+
+  }
+
+
+  answerDisplay.textContent = currentAnswer;
+
+}
+
+
+
+// ================================
+// 答えを表示
+// ================================
+
+answerBox.addEventListener("click", () => {
+
+  if (answerVisible) {
+    return;
+  }
+
+
+  answerVisible = true;
+
+
+  clickText.classList.add("hidden");
+
+  answerDisplay.classList.remove("hidden");
+
+  nextButton.classList.remove("hidden");
+
+});
+
+
+
+// ================================
+// 次の問題
+// ================================
+
+nextButton.addEventListener("click", () => {
+
+  generateQuestion();
+
+});
+
+
+
+// ================================
+// 動詞を変更したら新しい問題
+// ================================
+
+verbSelect.addEventListener("change", () => {
+
+  generateQuestion();
+
+});
+
+
+
+// ================================
+// 最初の問題
+// ================================
+
+generateQuestion();
